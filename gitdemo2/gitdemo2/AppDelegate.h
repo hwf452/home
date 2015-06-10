cbd56a238c409e6fb9f0eba26bc56e5b48aaf978
harry <hwf452@163.com>
2015-06-10 22:21:04 +0800

    0610

---
+++ gitdemo2/gitdemo2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitdemo2
+//
+//  Created by edao on 15-6-10.
+//  Copyright (c) 2015年 edao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitdemo2/gitdemo2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitdemo2
+//
+//  Created by edao on 15-6-10.
+//  Copyright (c) 2015年 edao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
