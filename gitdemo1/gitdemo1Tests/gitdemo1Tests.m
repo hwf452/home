49e31ab3891dee65d891dfb3f39eeda58b09fda4
harry <hwf452@163.com>
2015-06-10 22:03:12 +0800

    Initial Commit

---
+++ gitdemo1/gitdemo1Tests/gitdemo1Tests.m
@@ -0,0 +1,40 @@
+//
+//  gitdemo1Tests.m
+//  gitdemo1Tests
+//
+//  Created by edao on 15-6-10.
+//  Copyright (c) 2015年 edao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface gitdemo1Tests : XCTestCase
+
+@end
+
+@implementation gitdemo1Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
