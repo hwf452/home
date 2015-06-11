fc4dce7f23d8768075d80c8cf860105dab21cb02
harry <hwf452@163.com>
2015-06-11 12:09:13 +0800

    sdfsdfddf

--- MJExtension/MJExtensionExample/Status.m
+++ MJExtension/MJExtensionExample/Status.m
@@ -2,8 +2,7 @@
 //  Status.m
 //  字典与模型的互转
 //
-//  Created by MJ Lee on 14-5-21.sdfs
-//  Copyright (c) 2014年 itcast. All rights reserved.
+//  Created by MJ Lee on 14-5-21.sdfssdfdsts reserved.
 //ss
 
 #import "Status.h"
--- MJExtension/MJExtensionExample/StatusResult.m
+++ MJExtension/MJExtensionExample/StatusResult.m
@@ -1,7 +1,7 @@
 //
 //  StatusResult.m
 //  字典与模型的互转
-//ss
+//ssdsfds
 //  Created by MJ Lee on 14-5-21.
 //  Copyright (c) 2014年 itcast. All rights reserved.
 //
