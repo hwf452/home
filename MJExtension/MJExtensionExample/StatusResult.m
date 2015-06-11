c869fe651624e5b96b2bf2ed1371d4dc133863c4
harry <hwf452@163.com>
2015-06-11 12:28:34 +0800

    dsgsd

--- MJExtension/MJExtensionExample/StatusResult.m
+++ MJExtension/MJExtensionExample/StatusResult.m
@@ -1,15 +1,15 @@
 //
 //  StatusResult.m
-<<<<<<< HEAD
+
 //  字典与模型的互转
 //ssdsfdssdfds
 //  Created by MJ Lee on 14-5-21.sdfds
-=======
+
 //  字典与模型的互转sdfsdfsdfs
 //ssdsfds
-//  Created by MJ Lee on 14-5-21.
->>>>>>> master
-//  Copyright (c) 2014年 itcast. All rights reserved.
+//  Created by MJ Lee on 14-5-21.sdf
+
+//  Copyright (c) 2014年 itsfcast. All rights reserved.
 //
 
 #import "StatusResult.h"
